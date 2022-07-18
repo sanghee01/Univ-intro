3051714ff775fa3e4221fdb5d97d14875838550f
Yuminyumin <tlsdbalsdbal@naver.com>
2022-07-18 16:43:59 +0900

    npc 좌표 설정 완료
    npc %비율로 설정

---
+++ js/player2.js
@@ -0,0 +1,8 @@
+const npc1Move = document.getElementById('npc1');
+npc1Move.addEventListener('click',move1);
+
+
+
+$(document).ready( function() {
+    $('player').offset( { right: 1000, top: 2600 } );
+});
