e510e29a697ade93df9a0a09f55a7d147a3210d0
shy02 <85939242+shy02@users.noreply.github.com>
2022-07-19 20:40:05 +0900

    bgm추가

--- js/start.js
+++ js/start.js
@@ -5,7 +5,7 @@ const scrolling = document.getElementById("scroll");
 const map = document.getElementById("map");
 const player_image = document.getElementById("player");
 const upside=document.getElementById("upside");
-
+const backmusic = document.getElementById("bgm");
 
 const clickbutton = () =>{
     //없애기
@@ -15,11 +15,13 @@ const clickbutton = () =>{
 
     map.style.display="block";
     player_image.style.display="block";
-//  upside.style.display="block";
+    upside.style.display="block";
+    backmusic.style.display="block";
     //스크롤바 보이게 하기
     scrolling.style.overflow="visible";
     scrollTo({left:0, top:1935, behavior:"smooth"});
     //원하는위치까지 스크롤
+    backmusic.play();
 }
 
 startButton.addEventListener("click", clickbutton);
