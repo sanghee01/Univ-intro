361bdfbe3a81078ce4d662751fc509f37f9bfb3b
sanghee01 <sang6299@naver.com>
2022-07-19 14:01:45 +0900

    스크롤 위치 조정

--- js/start.js
+++ js/start.js
@@ -3,11 +3,7 @@ const box = document.getElementById("box");
 const start_overlay = document.getElementById("start_overlay");
 const scrolling = document.getElementById("scroll");
 const map = document.getElementById("map");
-
-//스크롤바 보이게 하기
-scrolling.style.overflow = "visible";
-scrollTo({ left: 0, top: 1470, behavior: "smooth" });
-//원하는위치까지 스크롤
+const player_image = document.getElementById("player");
 
 const clickbutton = () => {
   //없애기
@@ -16,9 +12,10 @@ const clickbutton = () => {
   start_overlay.style.display = "none";
 
   map.style.display = "block";
+  player_image.style.display = "block";
   //스크롤바 보이게 하기
   scrolling.style.overflow = "visible";
-  scrollTo({ left: 0, top: 1935, behavior: "smooth" });
+  scrollTo({ left: 0, top: 1405, behavior: "smooth" });
   //원하는위치까지 스크롤
 };
 
