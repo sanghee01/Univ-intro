053e27ea957008b175dc3dc8a67ee16b99211b0a
sanghee01 <sang6299@naver.com>
2022-07-19 12:53:29 +0900

    npc 위치 px로 고정 및 느낌표, npc 위치배열 수정

--- js/start.js
+++ js/start.js
@@ -3,18 +3,16 @@ const box = document.getElementById("box");
 const start_overlay = document.getElementById("start_overlay");
 const scrolling = document.getElementById("scroll");
 
+const clickbutton = () => {
+  //없애기
+  box.style.display = "none";
+  startButton.style.display = "none";
+  start_overlay.style.display = "none";
 
-
-const clickbutton = () =>{
-    //없애기
-    box.style.display="none";
-    startButton.style.display="none";
-    start_overlay.style.display="none";
-
-    //스크롤바 보이게 하기
-    scrolling.style.overflow="visible";
-    scrollTo({left:0, top:1935, behavior:"smooth"});
-    //원하는위치까지 스크롤
-}
+  //스크롤바 보이게 하기
+  scrolling.style.overflow = "visible";
+  scrollTo({ left: 0, top: 1470, behavior: "smooth" });
+  //원하는위치까지 스크롤
+};
 
 startButton.addEventListener("click", clickbutton);
