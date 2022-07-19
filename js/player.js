db078256b5411e5fc4c36acc20e10020670ef92a
Yuminyumin <tlsdbalsdbal@naver.com>
2022-07-20 01:01:25 +0900

    aa

--- js/player.js
+++ js/player.js
@@ -30,10 +30,12 @@ const dialogList = {
   0: {
     name: "미대생",
     script: [
-      "안녕? 거기서 뭐하니",
-      "여기는 뭐하는데야?",
-      "여기는 대학본부이야~",
-      "그렇구나!",
+      "안녕, 이곳은 대학 본부야",
+      "본부는 어떤 일을 하니?",
+      "이곳에서는 대학의 내부적인 운영이 이루어져",
+      "그렇구나",
+      "궁금한게 더 남아있니?",
+      "-",
       "https://www.wku.ac.kr/about/institutions/offices.html",
     ],
     img: "../img/대학본부.png",
@@ -41,10 +43,12 @@ const dialogList = {
   1: {
     name: "인문대생",
     script: [
-      "못보던 고양이네?",
-      "대학 구경 왔어",
-      "여기는 학생회관이야~ 1층엔 뭐가있고 2층엔 ~",
-      "고마워!",
+      "안녕, 이곳은 학생회관이야.",
+      "학생회관을 소개해줄래?",
+      "1층에는 서점과 식당들, 2층에는 학생 복지시설이 있어.",
+      "그럼 나머지층은?",
+      "3층에는 학생자치기구들이 있고, 4~5층에는 동아리 방들이 있어. 더 궁금한게 있니~?",
+      "-",
       "https://www.youtube.com/watch?v=UKAspu8Lz_k",
     ],
     img: "../img/학생회관.png",
@@ -52,10 +56,12 @@ const dialogList = {
   2: {
     name: "공대생",
     script: [
-      "끄어어...",
-      "왜그래??",
-      "여기는 프라임관이야 창의공과대학이 모여있어.",
-      "그렇구나! 힘내!",
+      "안녕, 이곳은 프라임관이야.",
+      "프라임관을 소개해줄래?",
+      "창의공과대학 학생들이 많이 이용하는 건물이야. ",
+      "건물이 깨끗하네? 건축한지 얼마 안됐니?",
+      "소프트웨어 중심대학에 선정되어서 새로 지었어.",
+      "-",
       "https://www.youtube.com/watch?v=F2b7d18Sl4Y",
     ],
     img: "../img/프라임관.png",
@@ -63,10 +69,12 @@ const dialogList = {
   3: {
     name: "자연대생",
     script: [
-      "넌 열성유전자를 갖고있군",
-      "뭐라는거야",
-      "아! 오늘 유전자에 대해 배웠거든 ㅎㅎ 난 휴식하러 수덕호에 왔어~",
-      "물 색깔이 정말 푸르다!",
+      "안녕, 이곳은 우리학교의 랜드마크 수덕호야!",
+      "풍경이 멋지다. 그 커피는 어디서 산거야?",
+      "수덕호 가운데에 카페가 있어, 거기서 산거야.",
+      "맛있겠다!",
+      "맛있어! 수덕호에 관심있으면 더 알려줄까?",
+      "-",
       "https://www.youtube.com/watch?v=2_j5GXY7624",
     ],
     img: "../img/수덕호.png",
@@ -74,10 +82,12 @@ const dialogList = {
   4: {
     name: "교수님",
     script: [
-      "엣헴",
-      "엇.. 교수님?!",
-      "여기는 학생지원관이다.",
-      "그렇군요",
+      "그래 원냥이구나",
+      "여기는 어디인가요?",
+      "이곳은 학생지원관이란다.",
+      "그렇군요, 무엇을 하나요?",
+      "이곳은 각종 행사를 진행하며 학생들이 다양한 경험을 하게 도와주는 곳이지.",
+      "-",
       "https://www.youtube.com/watch?v=ZqWBvbJJrbo",
     ],
     img: "../img/학생지원관.png",
@@ -85,10 +95,12 @@ const dialogList = {
   5: {
     name: "의대생",
     script: [
-      "어쩌구저쩌구(공부중)",
-      "앗.. 여기는 딱봐도 중앙도서관이군..",
-      "어쩌구저쩌구(공부중)",
-      "오...",
+      "어? 아.. 여기는 중앙 도서관이야",
+      "도서관이 뭔데?",
+      "학술 정보를 제공하기 위한 자료들이 존재하지",
+      "여기는 아무나 이용할 수 있어?",
+      "아니, 원대 학생이거나 회원증이 있는 사람만 이용할 수 있어",
+      "-",
       "https://elibrary.wku.ac.kr/lib/SlimaPlus.csp#link",
     ],
     img: "../img/중앙도서관.jpg",
@@ -140,7 +152,7 @@ const nextTalk = () => {
   talkScript[npcIndex].textContent = dialog.script[talkIndex];
   talkIndex++;
 
-  if (talkIndex === dialog.script.length - 1) {
+  if (talkIndex === dialog.script.length-1) {
     talkScript[npcIndex].textContent = "안녕! 다시 물어보러왔어!";
     groundheader.href = dialogList[npcIndex].script[talkIndex];
     groundheader.style.backgroundImage = `url(${dialogList[npcIndex].img})`;
