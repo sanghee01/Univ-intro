50e60badd69d2287335753e71c1211a5dad08af2
shy02 <85939242+shy02@users.noreply.github.com>
2022-07-19 13:28:10 +0900

    시작화면 고양이 안보이게

--- js/start.js
+++ js/start.js
@@ -3,6 +3,7 @@ const box = document.getElementById("box");
 const start_overlay = document.getElementById("start_overlay");
 const scrolling = document.getElementById("scroll");
 const map = document.getElementById("map");
+const player_image = document.getElementById("player");
 
 
 const clickbutton = () =>{
@@ -12,6 +13,7 @@ const clickbutton = () =>{
     start_overlay.style.display="none";
 
     map.style.display="block";
+    player_image.style.display="block";
     //스크롤바 보이게 하기
     scrolling.style.overflow="visible";
     scrollTo({left:0, top:1935, behavior:"smooth"});
