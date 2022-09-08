5814896908c0b2d4aa88c7cbe57a1068fae14f2d
sanghee01 <sang6299@naver.com>
2022-09-09 00:29:54 +0900

    지도 경로 수정

--- js/map.js
+++ js/map.js
@@ -2,6 +2,7 @@ const mapLocation = document.querySelector("#map");
 const npcs = document.querySelectorAll(".human");
 let npcLocation = 0;
 let locationY = [];
+const mapImage = [""];
 scrollTo(0, 0);
 
 for (let npc of npcs) {
@@ -13,18 +14,18 @@ addEventListener("scroll", () => {
   const scrollY = this.scrollY;
 
   if (scrollY > locationY[0] - 1000 && scrollY <= locationY[1] - 1000) {
-    mapLocation.style.backgroundImage = `url(${"../img/Unimap_bonboo.png"}`;
+    mapLocation.style.backgroundImage = `url(${"img/Unimap_bonboo.png"}`;
   } else if (scrollY >= locationY[1] - 1000 && scrollY <= locationY[2] - 1000) {
-    mapLocation.style.backgroundImage = `url(${"../img/Unimap_student.png"}`;
+    mapLocation.style.backgroundImage = `url(${"img/Unimap_student.png"}`;
   } else if (scrollY >= locationY[2] - 1000 && scrollY <= locationY[3] - 1000) {
-    mapLocation.style.backgroundImage = `url(${"../img/Unimap_prime.png"}`;
+    mapLocation.style.backgroundImage = `url(${"img/Unimap_prime.png"}`;
   } else if (scrollY >= locationY[3] - 1000 && scrollY <= locationY[4] - 1000) {
-    mapLocation.style.backgroundImage = `url(${"../img/Unimap_sudeokho.png"}`;
+    mapLocation.style.backgroundImage = `url(${"img/Unimap_sudeokho.png"}`;
   } else if (scrollY >= locationY[4] - 1000 && scrollY <= locationY[5] - 1000) {
-    mapLocation.style.backgroundImage = `url(${"../img/Unimap_Surpport.png"}`;
+    mapLocation.style.backgroundImage = `url(${"img/Unimap_Surpport.png"}`;
   } else if (scrollY >= locationY[5] - 1000) {
-    mapLocation.style.backgroundImage = `url(${"../img/Unimap_Library.png"}`;
+    mapLocation.style.backgroundImage = `url(${"img/Unimap_Library.png"}`;
   } else {
-    mapLocation.style.backgroundImage = `url(${"../img/Unimap.png"}`;
+    mapLocation.style.backgroundImage = `url(${"img/Unimap.png"}`;
   }
 });
