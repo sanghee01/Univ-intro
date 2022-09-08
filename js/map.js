3a360cbc7aa72971f03b01b395352e608fa4ba73
sanghee01 <sang6299@naver.com>
2022-09-08 21:43:19 +0900

    github page 지도 이미지 안뜸 현상 고치기 TEST

--- js/map.js
+++ js/map.js
@@ -13,18 +13,18 @@ addEventListener("scroll", () => {
   const scrollY = this.scrollY;
 
   if (scrollY >= locationY[0] && scrollY <= locationY[1]) {
-    mapLocation.src = "img/Unimap_bonboo.png";
+    mapLocation.src = "./img/Unimap_bonboo.png";
   } else if (scrollY >= locationY[1] && scrollY <= locationY[2]) {
-    mapLocation.src = "img/Unimap_student.png";
+    mapLocation.src = "./img/Unimap_student.png";
   } else if (scrollY >= locationY[2] && scrollY <= locationY[3]) {
-    mapLocation.src = "img/Unimap_prime.png";
+    mapLocation.src = "./img/Unimap_prime.png";
   } else if (scrollY >= locationY[3] && scrollY <= locationY[4]) {
-    mapLocation.src = "img/Unimap_sudeokho.png";
+    mapLocation.src = "./img/Unimap_sudeokho.png";
   } else if (scrollY >= locationY[4] && scrollY <= locationY[5]) {
-    mapLocation.src = "img/Unimap_Surpport.png";
+    mapLocation.src = "./img/Unimap_Surpport.png";
   } else if (scrollY >= locationY[5]) {
-    mapLocation.src = "img/Unimap_Library.png";
+    mapLocation.src = "./img/Unimap_Library.png";
   } else {
-    mapLocation.src = "img/Unimap.png";
+    mapLocation.src = "./img/Unimap.png";
   }
 });
