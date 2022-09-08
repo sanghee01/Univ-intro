00fcf4d2ca0ae969e6d4cae5be2c5c9ec131a983
sanghee01 <sang6299@naver.com>
2022-09-08 22:14:51 +0900

    대화 부분  리팩토링

--- js/player.js
+++ js/player.js
@@ -1,7 +1,3 @@
-window.onbeforeunload = function () {
-  window.scrollTo(0, 0);
-};
-
 const player = document.querySelector("#player");
 const npcList = document.querySelectorAll(".npc");
 const talk = document.querySelector(".talk");
@@ -32,11 +28,10 @@ const dialogList = {
       "이곳에서는 대학의 내부적인 운영이 이루어져",
       "그렇구나",
       "궁금한게 더 남아있니?",
-      "-",
-      "https://www.wku.ac.kr/about/institutions/offices.html",
     ],
     img: "img/대학본부.png",
     npcImg: "img/npc1.png",
+    url: "https://www.wku.ac.kr/about/institutions/offices.html",
   },
   1: {
     name: "인문대생",
@@ -46,11 +41,10 @@ const dialogList = {
       "1층에는 서점과 식당들, 2층에는 학생 복지시설이 있어.",
       "그럼 나머지층은?",
       "3층에는 학생자치기구들이 있고, 4~5층에는 동아리 방들이 있어. 더 궁금한게 있니~?",
-      "-",
-      "https://www.youtube.com/watch?v=UKAspu8Lz_k",
     ],
     img: "img/학생회관.png",
     npcImg: "img/npc2.png",
+    url: "https://www.youtube.com/watch?v=UKAspu8Lz_k",
   },
   2: {
     name: "공대생",
@@ -60,11 +54,10 @@ const dialogList = {
       "창의공과대학 학생들이 많이 이용하는 건물이야. ",
       "건물이 깨끗하네? 건축한지 얼마 안됐니?",
       "소프트웨어 중심대학에 선정되어서 새로 지었어.",
-      "-",
-      "https://www.youtube.com/watch?v=F2b7d18Sl4Y",
     ],
     img: "img/프라임관.png",
     npcImg: "img/npc3.png",
+    url: "https://www.youtube.com/watch?v=F2b7d18Sl4Y",
   },
   3: {
     name: "자연대생",
@@ -74,11 +67,10 @@ const dialogList = {
       "수덕호 가운데에 카페가 있어, 거기서 산거야.",
       "맛있겠다!",
       "맛있어! 수덕호에 관심있으면 더 알려줄까?",
-      "-",
-      "https://www.youtube.com/watch?v=2_j5GXY7624",
     ],
     img: "img/수덕호.png",
     npcImg: "img/npc4.png",
+    url: "https://www.youtube.com/watch?v=2_j5GXY7624",
   },
   4: {
     name: "교수님",
@@ -88,11 +80,10 @@ const dialogList = {
       "이곳은 학생지원관이란다.",
       "그렇군요, 무엇을 하나요?",
       "이곳은 각종 행사를 진행하며 학생들이 다양한 경험을 하게 도와주는 곳이지.",
-      "-",
-      "https://www.youtube.com/watch?v=ZqWBvbJJrbo",
     ],
     img: "img/학생지원관.png",
     npcImg: "img/npc5.png",
+    url: "https://www.youtube.com/watch?v=ZqWBvbJJrbo",
   },
   5: {
     name: "의대생",
@@ -102,11 +93,10 @@ const dialogList = {
       "학술 정보를 제공하기 위한 자료들이 존재하지",
       "여기는 아무나 이용할 수 있어?",
       "아니, 원대 학생이거나 회원증이 있는 사람만 이용할 수 있어",
-      "-",
-      "https://elibrary.wku.ac.kr/lib/SlimaPlus.csp#link",
     ],
     img: "img/중앙도서관.png",
     npcImg: "img/npc6.png",
+    url: "https://elibrary.wku.ac.kr/lib/SlimaPlus.csp#link",
   },
 };
 
@@ -115,8 +105,8 @@ let npcIndex = 0;
 // 클릭한 npc 인식 및 그 npc로 대사 설정, 대화창 띄움
 npcList.forEach((npc, index) => {
   npc.addEventListener("click", () => {
-    audio.play(); // 오디오 플레이
-    dialog = dialogList[index]; // 추후 close에서 수정해야하나? 근데 실행할때마다 해서 상관업을 것 같기도
+    audio.play(); // "냐옹" 효과음 플레이
+    dialog = dialogList[index]; // 클릭한 npc index
     // console.dir(dialog);
     npcIndex = index;
     openTalk(npcIndex);
@@ -135,7 +125,7 @@ const openTalk = (npcIndex) => {
   player.classList.add("hidden");
   nextBtn.addEventListener("click", nextTalk);
   talkNpcName.textContent = dialog.name; // 클릭한 npc 이름 설정
-  talkNpcImg.src = dialogList[npcIndex].npcImg; // 클린한 npc 이미지 설정
+  talkNpcImg.src = dialogList[npcIndex].npcImg; // 클릭한 npc 이미지 설정
   //overlay[npcIndex].addEventListener("click", closeTalk);
 };
 let talkIndex = 0;
@@ -156,9 +146,9 @@ const nextTalk = () => {
   talkScript.textContent = dialog.script[talkIndex];
   talkIndex++;
 
-  if (talkIndex === dialog.script.length - 1) {
+  if (talkIndex === dialog.script.length + 1) {
     talkScript.textContent = "안녕! 이 건물은 뭐하는 곳이야?";
-    groundheader.href = dialogList[npcIndex].script[talkIndex];
+    groundheader.href = dialogList[npcIndex].url;
     groundheader.style.backgroundImage = `url(${dialogList[npcIndex].img})`;
     talkIndex = 0;
     closeTalk();
@@ -177,4 +167,3 @@ const clicklink = () => {
   ground.classList.add("hidden");
 };
 ground.addEventListener("click", clicklink);
-//ground[npcIndex].classList.remove("hidden");
